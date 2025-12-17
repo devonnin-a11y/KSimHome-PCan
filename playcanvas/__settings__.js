afdcec5cfa5e265821c75bfc1e3d43929105bbb1
devonnin-a11y <devonnin@gmail.com>
2025-12-16 22:46:30 -0500

    Add files via upload

---
+++ playcanvas/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2386787.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 267989762, 267990293 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/267990149/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/267990150/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/267990148/1/ammo.js', 'preload' : true},
+];
